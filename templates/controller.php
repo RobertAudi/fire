642f4c9045308ddfa71d2266a4207b11464e63ca
Aziz Light <aziz@azizlight.me>
2011-05-13 19:40:36 +0300

    Added first version of the controller template

---
+++ templates/controller.php
@@ -0,0 +1,21 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * {{class_name}}
+ */
+class {{class_name}} extends {{parent_class}}
+{
+
+  /**
+   * The Contructor!
+   */
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  {{actions}}
+} // End of the {{class_name}} controller
+
+/* End of file {{filename}} */
+/* Location {{application_folder}}/controllers/{{filename}} */
