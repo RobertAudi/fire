af50ba958b6d1d38fb6307c35ba43f28194f328c
Aziz Light <aziz@azizlight.me>
2012-08-11 20:27:19 +0100

    If Fire is bootstrapped the new_project command will copy the sample CodeIgniter project instead of cloning from Github everytime

--- classes/commands/new_project.php
+++ classes/commands/new_project.php
@@ -42,13 +42,18 @@ class NewProject extends BaseCommand
     {
         $this->name = $args['name'];
         $this->location = getcwd() . DIRECTORY_SEPARATOR . $args['name'];
-        $this->repo = 'git://github.com/' . $args['github_repo'] . '.git';
 
-        // Check that git is installed
-        exec('which git > /dev/null 2>&1 && echo "FOUND" || echo "NOT_FOUND"', $output);
-        if ($output[0] === "NOT_FOUND")
+        // If fire is not bootstrapped, then we need to get CodeIgniter from the Inter-Webs
+        if (!$this->is_fire_bootstrapped())
         {
-            throw new RuntimeException("Git is required to create a new CodeIgniter project");
+            $this->repo = 'git://github.com/' . $args['github_repo'] . '.git';
+
+            // Check that git is installed
+            exec('which git > /dev/null 2>&1 && echo "FOUND" || echo "NOT_FOUND"', $output);
+            if ($output[0] === "NOT_FOUND")
+            {
+                throw new RuntimeException("Git is required to create a new CodeIgniter project");
+            }
         }
     }
 
@@ -61,14 +66,120 @@ class NewProject extends BaseCommand
      **/
     public function run()
     {
-        // First let's download CodeIgniter
-        if (GithubHelpers::git_clone($this->repo, $this->location) === FALSE)
+        if ($this->is_fire_bootstrapped())
         {
-            throw new RuntimeException("Unable to clone CodeIgniter from Github");
+            if ($this->copy_codeigniter_sample_project($this->location, BASE_PATH . '/codeigniter'))
+            {
+                fwrite(STDOUT, "\t" . ApplicationHelpers::colorize('CodeIgniter project created', 'green') . ' ' . $this->name . "\n");
+            }
+            else
+            {
+                throw new RuntimeException("Unable to create a new CodeIgniter Project");
+            }
         }
         else
         {
-            fwrite(STDOUT, "\t" . ApplicationHelpers::colorize('CodeIgniter project created', 'green') . ' ' . $this->name . "\n");
+            // First let's download CodeIgniter
+            if (GithubHelpers::git_clone($this->repo, $this->location) === FALSE)
+            {
+                throw new RuntimeException("Unable to clone CodeIgniter from Github");
+            }
+            else
+            {
+                fwrite(STDOUT, "\t" . ApplicationHelpers::colorize('CodeIgniter project created', 'green') . ' ' . $this->name . "\n");
+            }
         }
+    }
+
+    /**
+     * Copy the CodeIgniter folder to create a new project.
+     * It's a pain in the ass to transfer permissions when copying a file/folder
+     * in php so here is what will happen:
+     *     - All the files will have a permission of 644.
+     *     - All the folders will have a permission of 755.
+     *
+     * I did not create this method, It's Sina Salek who left it as comment
+     * on php.net {@link http://www.php.net/manual/en/function.copy.php#91256}
+     * and then info ]t[ intalo [.] de modified
+     * it {@link http://www.php.net/manual/en/function.copy.php#93953}
+     *
+     * @access private
+     * @param string $dest : The destination folder.
+     * @param string $source : The source file/folder.
+     * @param string $folderPermission : The permission that will be given to all folders.
+     * @param string $filePermission  : The permission that will be given to all files.
+     * @return bool Whether or not the copying succeeded.
+     * @author Sina Salek
+     */
+    private function copy_codeigniter_sample_project($dest = '', $source = self::codeigniter_path, $folderPermission = 0755, $filePermission = 0644)
+    {
+        $result = FALSE;
+
+        if (is_file($source))
+        {
+            if (is_dir($dest))
+            {
+                if ($dest[strlen($dest) - 1] != '/')
+                {
+                    $__dest = $dest . '/';
+                }
+
+                $__dest .= basename($source);
+            }
+            else
+            {
+                $__dest = $dest;
+            }
+
+            $result = copy($source, $__dest);
+            chmod($__dest, $filePermission);
+        }
+        elseif (is_dir($source))
+        {
+            if (!is_dir($dest))
+            {
+                mkdir($dest, $folderPermission);
+                chmod($dest, $folderPermission);
+            }
+
+            if ($source[strlen($source) - 1] != '/')
+            {
+                $source = $source . '/';
+            }
+
+            if ($dest[strlen($dest) - 1] != '/')
+            {
+                $dest = $dest . '/';
+            }
+
+            $result = TRUE;
+            $dirHandle = opendir($source);
+            while ($file = readdir($dirHandle))
+            {
+                if ($file != '.' && $file != '..')
+                {
+                    $result = $this->copy_codeigniter_sample_project($dest . $file, $source . $file, $folderPermission, $filePermission);
+                }
+            }
+            closedir($dirHandle);
+        }
+        else
+        {
+            $result = FALSE;
+        }
+
+        return $result;
+    }
+
+    /**
+     * Check if Fire is bootstrapped or not
+     *
+     * @access private
+     * return bool Wether or not Fire is bootstrapped
+     * @author Aziz Light
+     */
+    private function is_fire_bootstrapped()
+    {
+        return is_dir(BASE_PATH . '/codeigniter');
     }
 }
