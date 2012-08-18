3c5f1933d60b82f982cd967d0174b33a84184551
Aziz Light <aziz@azizlight.me>
2012-08-18 22:15:33 +0100

    Added an id as primary key and created_at and updated_at date time fields to the migration template

--- templates/migration.php
+++ templates/migration.php
@@ -8,9 +8,25 @@ class {{class_name}} extends {{parent_class}}
     public function up()
     {
         $this->dbforge->add_field(array(
+            'id' => array(
+                'type' => 'INT',
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE
+            ),
+
 {{extra}}
+            'created_at' => array(
+                'type' => 'DATETIME',
+                'null' => FALSE
+            ),
+
+            'updated_at' => array(
+                'type' => 'DATETIME',
+                'null' => FALSE
+            )
         ));
 
+        $this->add_key('id', TRUE);
         $this->dbforge->create_table('{{table_name}}');
     }
 
